6af9ae3d8401ac6f6c42837a8a57a1919362ef53
XAVIERDUNIFON <xdunifon@gmail.com>
2022-12-20 22:02:23 -0500

    Combat Introduction

--- AdventureGame.py
+++ AdventureGame.py
@@ -1,6 +1,7 @@
 from CreatureClass import creature
 from GameStateClass import game_state
 import CreatureLibrary
+import LoopStates
 
 '''
 THIS IS THE MAIN PROFILE THAT THE GAME RUNS UNDER.
@@ -10,14 +11,16 @@ IT RELIES ON OTHER CLASSES TO PROVIDE GAMEPLAY STRUCTURE AND LOOPS.
 player_name = input("Enter your character's name:")
 while len(player_name) > 18:
     player_name = input("Enter a name that's less than 19 characters: ")
-Player = creature(player_name, 10, 10, False, False, 10)
+Player = creature(player_name, 10, 5, False, False, 10)
 print(Player.get_all())
 
 gstate = game_state(False, True, "Forest", False)
-#goblin = CreatureLibrary.create_goblin(1)
+goblin1 = CreatureLibrary.create_goblin(1)
+goblin2 = CreatureLibrary.create_goblin(2)
+goblin3 = CreatureLibrary.create_goblin(3)
 #print(goblin.get_all())
-
-
+enemies = [goblin1, goblin2, goblin3]
+LoopStates.combat_loop(Player, enemies)
 
 exitMsg = input("Enter 'exit' to quit: ")
 while exitMsg != "exit":
--- LoopStates.py
+++ LoopStates.py
@@ -13,19 +13,58 @@ def all_dead(creatures):
     return True
 
 #Main loop for combat scenarios
-def combat_loop(enemies):
-    while not all_dead(enemies):
-        print("You're under attack! Select a target and take action")
+def combat_loop(player, enemies):
+    player_turn = True
+    print("You're under attack!")
+
+    while len(enemies) > 0:
+        print("\n" + player.get_all())
+        print("It's your turn, select a target first then take action")
         
         for i, x in enumerate(enemies):
-            print(f"{i+1}. {x}")
-        target = input("\nTarget (Number): ")
+            print(f"{i+1}. {x.get_name()} ({x.get_dmg()} HP)")
+        target = int(input("\nTarget (Number): "))-1
         
-        while target < 0 or target > len(enemies)+1:
+        while target < 0 or target >= len(enemies):
             print("Uhhhh.. are you sure you entered the right number? Try again")
             for i, x in enumerate(enemies):
-                print(f"{i+1}. {x}")
-            target = input("\nTarget (Number): ")
+                print(f"{i+1}. {x.get_name()} ({x.get_dmg()} HP)")
+            target = int(input("\nTarget (Number): "))-1
         
-        action = input(f"You're targeting {enemies[target]}. What action will you take?\n1. Attack\n2. Compliment\n3. Run\n4. Give up\n\nEnter option number: ")
+        enemy_name = enemies[target].get_name()
+        action = input(f"\nYou're targeting {enemy_name}. What action will you take?\n1. Attack\n2. Compliment\n3. Run\n4. Quit\n\nEnter option number: ")
         
+        while player_turn == True:
+            match action:
+                case "1":
+                    print(f"You attack {enemy_name} for {str(player.get_dmg())} damage")
+                    if enemies[target].damage(player.get_dmg()) <= 0:
+                        print(f"Good work, {enemy_name} is down")
+                        enemies.pop(target)
+                    player_turn = False
+                    break
+                case "2":
+                    comp = input(f"What will you say to compliment {enemy_name}?: ")
+                    print(f"\nYou look to {enemy_name} and say: {comp}\n{enemy_name} blushes and their HP drops by {player.get_dmg()}")
+                    if enemies[target].damage(player.get_dmg()) <= 0:
+                        print(f"\nGood work! {enemy_name} is down")
+                        enemies.pop(target)
+                    player_turn = False
+                    break
+                case "3":
+                    print("You ran away")
+                    return 0
+                    break
+                case "4":
+                    print("You decided you don't wanna play anymore")
+                    exit()
+                    break
+                case _:
+                    action = input("I don't quite understand what you entered, try again: ")
+        
+        if len(enemies) > 0:
+            print("Now it's the enemies turn\n")
+            for x in enemies:
+                player.damage(x.get_dmg())
+                print(f"{x.get_name()} attacked you for {x.get_dmg()} damage")
+        player_turn = True
